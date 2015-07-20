1b0c700504f9e1869cd91988b23dc96b38963bbe
eugyev <eugyev@gmail.com>
2015-07-20 14:16:37 -0400

    change after initialize to after create which fixes bug

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,6 @@
 class Patient < ActiveRecord::Base
     belongs_to :service, inverse_of: :patients
-    after_initialize :defaults #this will call the 'defaults' method whenever a new entry is created
+    after_create :defaults #this will call the 'defaults' method whenever a new entry is created
     
     def defaults
       self.update_attributes(active?: true) if active? == nil
